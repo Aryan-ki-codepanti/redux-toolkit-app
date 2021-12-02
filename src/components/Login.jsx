5c63e84040ea65008443241ed02d346f55550016
Aryan Sethi <aryan.rdps@gmail.com>
2021-12-02 09:28:27 +0530

    Added redux state access in profile , dispatch in login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { login } from '../features/User';
 
 const Login = () => {
+    const dispatch = useDispatch();
     return (
         <div>
-            <button>Login</button>
+            <button onClick={() => dispatch(login({ name:"aryan" , email: "pedo@gmail.com" , age:3 }))}>Login</button>
         </div>
     )
 }

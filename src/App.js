d934eca4bb24e9818ce0ba10757b254f9a3e3029
Aryan Sethi <aryan.rdps@gmail.com>
2021-12-02 09:09:23 +0530

    Added profile and login component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
+import "./App.css";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
 
 function App() {
-  return (
-    <div className="App">
-      App
-    </div>
-  );
+    return (
+        <div className="App">
+            <Profile />
+            <Login />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Profile = () => {
+    return (
+        <div>
+            <h1>Profile page</h1>
+            <p>Name: </p>
+            <p>Email: </p>
+            <p>Age: </p>
+        </div>
+    )
+}
+
+export default Profile
